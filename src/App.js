bfe5d97978fdf4f7577d4cce0f2a3989276f3292
yeonsookang <yeonsoo.kang@lapiscine.pro>
2023-11-07 10:11:04 +0100

    my first changement on React

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const title = `c'est mon premiere phragraphe via React`
+  const h1 = "hello la piscine"
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{h1}</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <p>{title}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
