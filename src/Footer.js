5be276069f026111b9aeefc37c05af7aab41db01
yeonsookang <yeonsoo.kang@lapiscine.pro>
2023-11-07 11:33:55 +0100

    my first component

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+function Footer (){
+    return(
+        <footer className="footer">
+            <p>
+                &copy; CopyRight 2023 Yeonsoo Kang &nbsp; | &nbsp;  
+                <a href="">click here for more rights and polices.</a>
+                </p>
+        </footer>
+        
+    )
+}
+
+export default Footer
